64f9385f976fe8781053d32b74a1b1ccd89198db
SukkaW <isukkaw@gmail.com>
2023-08-10 17:04:36 +0800

    feat: add `typedarray-byte-offset`

---
+++ packages/typed-array-byte-offset/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { TypedArrayPrototype, uncurryThis } = require('@nolyfill/shared');
+const typedArrayByteOffSet = uncurryThis(Object.getOwnPropertyDescriptor(TypedArrayPrototype, 'byteOffset').get);
+
+module.exports = (value) => {
+  try {
+    return typedArrayByteOffSet(value);
+  } catch (e) {
+    return false;
+  }
+};
