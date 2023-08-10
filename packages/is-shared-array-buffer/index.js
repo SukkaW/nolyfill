ccf04c84b3a1783e7b897d12f169ffd48e3744b3
SukkaW <isukkaw@gmail.com>
2023-08-10 16:32:25 +0800

    feat: add `is-shared-array-buffer`

---
+++ packages/is-shared-array-buffer/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { uncurryThis } = require('@nolyfill/shared');
+const bL = uncurryThis(Object.getOwnPropertyDescriptor(SharedArrayBuffer.prototype, 'byteLength').get);
+
+module.exports = (obj) => {
+  if (!obj || typeof obj !== 'object') {
+    return false;
+  }
+
+  try {
+    bL(obj);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
