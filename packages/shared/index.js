d6690ffb35cb8907be9cd8fb6501b7b6732da587
SukkaW <isukkaw@gmail.com>
2023-08-10 16:42:15 +0800

    feat: add `TypedArrayPrototype` exports to `shared`

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,5 +1,7 @@
 'use strict';
 const { bind, call } = Function.prototype;
 const uncurryThis = bind.bind(call);
+const TypedArrayPrototype = Object.getPrototypeOf(Uint8Array.prototype);
 
 module.exports.uncurryThis = uncurryThis;
+module.exports.TypedArrayPrototype = TypedArrayPrototype;
