4894147e838d75291ae7f42b1758a657b6aba596
SukkaW <isukkaw@gmail.com>
2023-08-16 15:46:15 +0800

    chore: codegen `array-buffer-byte-length`

--- packages/array-buffer-byte-length/index.js
+++ packages/array-buffer-byte-length/index.js
@@ -1,12 +1,8 @@
 'use strict';
-
 const { uncurryThis } = require('@nolyfill/shared');
-const bL = uncurryThis(Object.getOwnPropertyDescriptor(ArrayBuffer.prototype, 'byteLength').get);
 const isArrayBuffer = require('@nolyfill/is-array-buffer');
-
+const bL = uncurryThis(Object.getOwnPropertyDescriptor(ArrayBuffer.prototype, 'byteLength').get);
 module.exports = (ab) => {
-  if (!isArrayBuffer(ab)) {
-    return NaN;
-  }
+  if (!isArrayBuffer(ab)) return NaN;
   return bL(ab);
 };
