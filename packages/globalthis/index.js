278a65f2dc5edadaacfbd1e34abd9870cf89a509
SukkaW <isukkaw@gmail.com>
2023-08-21 00:09:08 +0800

    fix: `gobalthis`

--- packages/globalthis/index.js
+++ packages/globalthis/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const { makeEsShim } = require('@nolyfill/shared');
 const impl = globalThis;
-const bound = impl;
+const bound = () => impl;
 makeEsShim(bound, impl);
 module.exports = bound;
