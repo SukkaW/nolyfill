15b09c747eba094e676f15895d496e4b50fd4173
kanno <812137533@qq.com>
2023-08-25 22:54:57 +0800

    feat: using core-js

--- packages/cli/src/package-manager.ts
+++ packages/cli/src/package-manager.ts
@@ -4,6 +4,8 @@ import fsp from 'fs/promises';
 
 import { fileExists } from '@nolyfill/internal';
 
+import _any from 'core-js-pure/features/promise/any';
+
 export type PackageManager = 'npm' | 'pnpm' | 'yarn' | 'bun';
 
 const checkFile = (path: PathLike) => fsp.access(path, fs.constants.F_OK);
@@ -15,18 +17,15 @@ export async function detectPackageManager(projectPath: string): Promise<Package
     throw new Error(`Failed to locate package.json at ${projectPath}. Are you sure the path is correct?`);
   }
 
-  const tasks:Array<Promise<PackageManager>> = [
-    checkFile(path.join(projectPath, 'yarn.lock')).then<'yarn'>(() => 'yarn'),
-    checkFile(path.join(projectPath, 'pnpm-lock.yaml')).then<'pnpm'>(() => 'pnpm'),
-    checkFile(path.join(projectPath, 'package-lock.json')).then<'npm'>(() => 'npm'),
-    checkFile(path.join(projectPath, 'npm-shrinkwrap.json')).then<'npm'>(() => 'npm'),
-    checkFile(path.join(projectPath, 'bun.lockb')).then<'bun'>(() => 'bun')
-  ];
-  return new Promise((resolve, reject) => {
-    let cap = tasks.length;
-    tasks.forEach((task) => task.then(v => resolve(v), _ => {
-      cap--;
-      if (!cap) reject(new Error('Can not determine the preferred package manager.'));
-    }));
-  });
+  try {
+    return await _any([
+      checkFile(path.join(projectPath, 'yarn.lock')).then<'yarn'>(() => 'yarn'),
+      checkFile(path.join(projectPath, 'pnpm-lock.yaml')).then<'pnpm'>(() => 'pnpm'),
+      checkFile(path.join(projectPath, 'package-lock.json')).then<'npm'>(() => 'npm'),
+      checkFile(path.join(projectPath, 'npm-shrinkwrap.json')).then<'npm'>(() => 'npm'),
+      checkFile(path.join(projectPath, 'bun.lockb')).then<'bun'>(() => 'bun')
+    ]);
+  } catch {
+    throw new Error('Can not determine the preferred package manager.');
+  }
 }
