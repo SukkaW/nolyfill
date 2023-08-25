3cbcf90e5d8c772ad8103fa56381f1cd7c9a27b9
SukkaW <isukkaw@gmail.com>
2023-08-26 00:54:52 +0800

    types: update cli type

--- packages/cli/src/global.d.ts
+++ packages/cli/src/global.d.ts
@@ -1,4 +1,4 @@
-declare module '@nolyfill/promise.any/implementation' {
+declare module '@nolyfill/promise.any' {
   function _any<T extends readonly unknown[] | []>(values: T): Promise<Awaited<T[number]>>;
   function _any<T>(values: Iterable<T | PromiseLike<T>>): Promise<Awaited<T>>;
   export default _any;
