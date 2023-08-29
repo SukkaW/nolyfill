378ec1aea29239d6ea4b86ae4fd3b4195ba36ac8
SukkaW <isukkaw@gmail.com>
2023-08-29 10:53:05 +0800

    refactor: make `side-channel` smaller

--- create.cjs
+++ create.cjs
@@ -430,51 +430,41 @@ module.exports = function isGeneratorFunction(fn) {
   return Object.getPrototypeOf(fn) === GeneratorFunction;
 }`],
   ['side-channel', `module.exports = () => {
-  let $wm;
-  let $m;
+  let $wm, $m;
 
-  const channel = {
-    assert(key) {
-      if (!channel.has(key)) {
-        throw new TypeError('Side channel does not contain the given key');
-      }
-    },
-    get(key) {
-      if (key && (typeof key === 'object' || typeof key === 'function')) {
-        if ($wm) {
-          return $wm.get(key);
-        }
-      } else if ($m) {
-        return $m.get(key);
-      }
-      return undefined;
-    },
-    has(key) {
-      if (key && (typeof key === 'object' || typeof key === 'function')) {
-        if ($wm) {
-          return $wm.has(key);
-        }
-      } else if ($m) {
-        return $m.has(key);
-      }
-      return false;
-    },
-    set(key, value) {
-      if (key && (typeof key === 'object' || typeof key === 'function')) {
-        if (!$wm) {
-          $wm = new WeakMap();
-        }
-        $wm.set(key, value);
-      } else {
-        if (!$m) {
-          $m = new Map();
-        }
-        $m.set(key, value);
-      }
+  const get = (key) => {
+    if (key && (typeof key === 'object' || typeof key === 'function')) {
+      if ($wm) return $wm.get(key);
+    } else if ($m) {
+      return $m.get(key);
+    }
+    return undefined;
+  };
+  const set = (key, value) => {
+    if (key && (typeof key === 'object' || typeof key === 'function')) {
+      if (!$wm) $wm = new WeakMap();
+      $wm.set(key, value);
+    } else {
+      if (!$m) $m = new Map();
+      $m.set(key, value);
     }
   };
-  // eslint-disable-next-line @fluffyfox/no-redundant-variable -- self-reference
-  return channel;
+  const has = (key) => {
+    if (key && (typeof key === 'object' || typeof key === 'function')) {
+      if ($wm) {
+        return $wm.has(key);
+      }
+    } else if ($m) {
+      return $m.has(key);
+    }
+    return false;
+  };
+  const assert = (key) => {
+    if (!has(key)) {
+      throw new TypeError('Side channel does not contain the given key');
+    }
+  };
+  return { get, set, has, assert };
 }`]
 ]);
 
