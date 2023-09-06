7ef6f43dcc232beb50b54ca16ca4e60b03440ab7
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-06 15:23:54 +0200

    chore: update

--- packages/tools/cli/rollup.config.ts
+++ packages/tools/cli/rollup.config.ts
@@ -74,17 +74,17 @@ export default async () => {
               let magicString: MagicString | null = null;
 
               /**
-             * Here we are making 3 assumptions:
-             * - shebang can only be at the first line of the file, otherwise it will not be recognized
-             * - shebang can only contains one line
-             * - shebang must starts with # and !
-             *
-             * Those assumptions are also made by acorn, babel and swc:
-             *
-             * - acorn: https://github.com/acornjs/acorn/blob/8da1fdd1918c9a9a5748501017262ce18bb2f2cc/acorn/src/state.js#L78
-             * - babel: https://github.com/babel/babel/blob/86fee43f499c76388cab495c8dcc4e821174d4e0/packages/babel-parser/src/tokenizer/index.ts#L574
-             * - swc: https://github.com/swc-project/swc/blob/7bf4ab39b0e49759d9f5c8d7f989b3ed010d81a7/crates/swc_ecma_parser/src/lexer/mod.rs#L204
-             */
+               * Here we are making 3 assumptions:
+               * - shebang can only be at the first line of the file, otherwise it will not be recognized
+               * - shebang can only contains one line
+               * - shebang must starts with # and !
+               *
+               * Those assumptions are also made by acorn, babel and swc:
+               *
+               * - acorn: https://github.com/acornjs/acorn/blob/8da1fdd1918c9a9a5748501017262ce18bb2f2cc/acorn/src/state.js#L78
+               * - babel: https://github.com/babel/babel/blob/86fee43f499c76388cab495c8dcc4e821174d4e0/packages/babel-parser/src/tokenizer/index.ts#L574
+               * - swc: https://github.com/swc-project/swc/blob/7bf4ab39b0e49759d9f5c8d7f989b3ed010d81a7/crates/swc_ecma_parser/src/lexer/mod.rs#L204
+               */
               // eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with -- [0] is way faster
               if (code[0] === '#' && code[1] === '!') {
                 let firstNewLineIndex = 0;
@@ -107,6 +107,8 @@ export default async () => {
                   };
                 }
               }
+
+              return null;
             }
           }
         },
