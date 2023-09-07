02d39cf70e0116c619fa5546572b2807d5ee90e8
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-07 14:46:22 +0200

    feat: basic JS API (#27)

--- packages/tools/cli/rollup.config.ts
+++ packages/tools/cli/rollup.config.ts
@@ -4,6 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { visualizer } from 'rollup-plugin-visualizer';
+import dts from 'rollup-plugin-dts';
 
 import fs from 'fs';
 import { builtinModules } from 'module';
@@ -29,104 +30,125 @@ export default async () => {
   ).concat(builtinModules);
   const external = (id: string) => dependencies.some((dep) => dep === id || (id.startsWith(`${dep}/`) && id !== `${dep}/`));
 
-  return defineConfig({
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: false,
-      esModule: false,
-      // graceful-fs requires patching fs exports, so we must allow fs exports to be modified
-      externalLiveBindings: false,
-      freeze: false
-    },
-    plugins: [
-      nodeResolve({
+  return defineConfig([
+    {
+      input: [
+        'src/index.ts',
+        'src/cli.ts'
+      ],
+      output: {
+        dir: 'dist',
+        format: 'cjs',
+        sourcemap: false,
+        esModule: false,
+        // graceful-fs requires patching fs exports, so we must allow fs exports to be modified
+        externalLiveBindings: false,
+        freeze: false
+      },
+      plugins: [
+        nodeResolve({
         // Use node like module resolution
-        exportConditions: ['node']
-      }),
-      commonjs({
-        esmExternals: true,
-        requireReturnsDefault(id) {
-          return builtinModulesSet.has(id);
-        }
-      }),
-      json(),
-      {
-        name: 'build-nolyfill-cli',
-        load(id) {
-          id = slash(id);
-          // Here we remove the query-selector-all.js from arborist,  as it introduces shit
-          // load of dependencies that we totally don't use
-          if (id.includes('/arborist/lib/query-selector-all.js')) {
-            return 'module.exports = () => {}';
+          exportConditions: ['node']
+        }),
+        commonjs({
+          esmExternals: true,
+          requireReturnsDefault(id) {
+            return builtinModulesSet.has(id);
           }
-          return undefined;
-        },
-        transform: {
-          order: 'pre',
-          handler(code) {
-            let magicString: MagicString | null = null;
+        }),
+        json(),
+        {
+          name: 'build-nolyfill-cli',
+          load(id) {
+            id = slash(id);
+            // Here we remove the query-selector-all.js from arborist,  as it introduces shit
+            // load of dependencies that we totally don't use
+            if (id.includes('/arborist/lib/query-selector-all.js')) {
+              return 'module.exports = () => {}';
+            }
+            return undefined;
+          },
+          transform: {
+            order: 'pre',
+            handler(code) {
+              let magicString: MagicString | null = null;
 
-            /**
-             * Here we are making 3 assumptions:
-             * - shebang can only be at the first line of the file, otherwise it will not be recognized
-             * - shebang can only contains one line
-             * - shebang must starts with # and !
-             *
-             * Those assumptions are also made by acorn, babel and swc:
-             *
-             * - acorn: https://github.com/acornjs/acorn/blob/8da1fdd1918c9a9a5748501017262ce18bb2f2cc/acorn/src/state.js#L78
-             * - babel: https://github.com/babel/babel/blob/86fee43f499c76388cab495c8dcc4e821174d4e0/packages/babel-parser/src/tokenizer/index.ts#L574
-             * - swc: https://github.com/swc-project/swc/blob/7bf4ab39b0e49759d9f5c8d7f989b3ed010d81a7/crates/swc_ecma_parser/src/lexer/mod.rs#L204
-             */
-            // eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with -- [0] is way faster
-            if (code[0] === '#' && code[1] === '!') {
-              let firstNewLineIndex = 0;
+              /**
+               * Here we are making 3 assumptions:
+               * - shebang can only be at the first line of the file, otherwise it will not be recognized
+               * - shebang can only contains one line
+               * - shebang must starts with # and !
+               *
+               * Those assumptions are also made by acorn, babel and swc:
+               *
+               * - acorn: https://github.com/acornjs/acorn/blob/8da1fdd1918c9a9a5748501017262ce18bb2f2cc/acorn/src/state.js#L78
+               * - babel: https://github.com/babel/babel/blob/86fee43f499c76388cab495c8dcc4e821174d4e0/packages/babel-parser/src/tokenizer/index.ts#L574
+               * - swc: https://github.com/swc-project/swc/blob/7bf4ab39b0e49759d9f5c8d7f989b3ed010d81a7/crates/swc_ecma_parser/src/lexer/mod.rs#L204
+               */
+              // eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with -- [0] is way faster
+              if (code[0] === '#' && code[1] === '!') {
+                let firstNewLineIndex = 0;
 
-              for (let i = 2, len = code.length; i < len; i++) {
-                const charCode = code.charCodeAt(i);
-                if (charCode === 10 || charCode === 13 || charCode === 0x2028 || charCode === 0x2029) {
-                  firstNewLineIndex = i;
-                  break;
+                for (let i = 2, len = code.length; i < len; i++) {
+                  const charCode = code.charCodeAt(i);
+                  if (charCode === 10 || charCode === 13 || charCode === 0x2028 || charCode === 0x2029) {
+                    firstNewLineIndex = i;
+                    break;
+                  }
+                }
+
+                if (firstNewLineIndex) {
+                  magicString ||= new MagicString(code);
+                  magicString.remove(0, firstNewLineIndex + 1);
+
+                  return {
+                    code: magicString.toString(),
+                    map: magicString.generateMap({ hires: true }).toMap()
+                  };
                 }
               }
 
-              if (firstNewLineIndex) {
-                magicString ||= new MagicString(code);
-                magicString.remove(0, firstNewLineIndex + 1);
-
-                return {
-                  code: magicString.toString(),
-                  map: magicString.generateMap({ hires: true }).toMap()
-                };
-              }
+              return null;
             }
           }
-        }
-      },
-      swc({
+        },
+        swc({
         // minify: true,
-        jsc: {
-          externalHelpers: true,
-          minify: {
+          jsc: {
+            externalHelpers: true,
+            minify: {
             // https://github.com/swc-project/swc/issues/7847
-            compress: false,
-            // compress: {
-            //   passes: 2,
-            //   sequences: false,
-            //   hoist_props: false
-            // },
-            mangle: true,
-            module: true
+              compress: false,
+              // compress: {
+              //   passes: 2,
+              //   sequences: false,
+              //   hoist_props: false
+              // },
+              mangle: true,
+              module: true
+            }
           }
-        }
-      }),
-      process.env.ANALYZE === 'true' && visualizer({
-        filename: 'dist/stats.html'
+        }),
+        process.env.ANALYZE === 'true' && visualizer({
+          filename: 'dist/stats.html'
         // template: 'network'
-      })
-    ],
-    external
-  });
+        })
+      ],
+      external
+    },
+    // Build types
+    {
+      input: 'src/index.ts',
+      output: {
+        file: 'dist/index.d.ts',
+        format: 'es'
+      },
+      plugins: [
+        dts({
+          respectExternal: true
+        })
+      ],
+      external
+    }
+  ]);
 };
---
+++ packages/tools/cli/src/cli.ts
@@ -0,0 +1,140 @@
+/* eslint-disable no-console -- We're a CLI, we need to log to the console */
+import path from 'path';
+import picocolors from 'picocolors';
+import { Command, Option } from 'commander';
+import handleError from './handle-error';
+import { detectPackageManager, type PackageManager } from './package-manager';
+import { renderTree } from './renderTree';
+
+import { overridesPackageJson } from './lib/json';
+import type { PKG } from './types';
+import { handleSigTerm } from './lib/handle-sigterm';
+import { findPackagesCoveredByNolyfill, findPackagesNotCoveredByNolyfill } from './find-coverable-packages';
+import { checkForUpdates } from './check-update';
+import { generateIssue } from './generate-issue';
+
+interface CliOptions {
+  /** see full error messages, mostly for debugging */
+  debug?: boolean
+}
+
+interface PmCommandOptions {
+  /** specify which package manager to use */
+  pm: PackageManager | 'auto'
+}
+
+const pmCommandOption = new Option('--pm [package manager]', 'specify which package manager to use')
+  .choices(['auto', 'npm', 'pnpm', 'yarn'])
+  .default('auto', 'detect package manager automatically');
+
+handleSigTerm();
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires -- version
+const { version } = require('../package.json') as PKG;
+
+const checkUnsupportedPM = (packageManager: PackageManager) => {
+  if (packageManager === 'bun') {
+    console.log(`${picocolors.bgRed(picocolors.black(' Error '))} nolyfill does not support ${picocolors.bold('Bun')} at the moment.\n`);
+    console.log(`Currently, ${picocolors.bold('Bun')} doesn't support package.json overrides (Details: ${picocolors.underline('https://github.com/oven-sh/bun/issues/1134')}). This feature is essential for nolyfill. We'll add support for ${picocolors.bold('Bun')} once the issue is addressed.\n`);
+    return true;
+  }
+  return false;
+};
+
+const printPostInstallInstructions = (packageManager: PackageManager) => {
+  console.log(`${picocolors.magenta('Almost complete! One last step:')}\n`);
+
+  switch (packageManager) {
+    case 'npm':
+      console.log(`${picocolors.dim('>')} Run "${picocolors.bold(picocolors.green('npm update'))}" to finish the optimization.\n`);
+      console.log(`${picocolors.bold(picocolors.bgYellow(picocolors.black(' WARNING ')))} Using ${picocolors.red('npm update')} will rebuild the entire package-lock.json file, potentially causing unwanted upgrades and side effects. Please review package versions and test your application thoroughly after updating.\n`);
+      console.log(`Due to a known bug in NPM (see ${picocolors.underline('https://github.com/npm/cli/issues/5850')}), you need to execute ${picocolors.green('npm update')} instead of the standard "npm install". This ensures NPM to respect the overrides added by nolyfill. We appreciate your understanding and are closely monitoring this issue for any resolutions.\n`);
+      break;
+    case 'pnpm':
+      console.log(`${picocolors.dim('>')} Run "${picocolors.bold(picocolors.green('pnpm install'))}" to finish the optimization.\n`);
+      break;
+    case 'yarn':
+      console.log(`${picocolors.dim('>')} Run "${picocolors.bold(picocolors.green('yarn install'))}" to finish the optimization.\n`);
+      break;
+    default:
+      break;
+  }
+};
+
+const program = new Command('nolyfill');
+(async () => {
+  try {
+    program
+      .version(version, '-v, --version', 'output the current version')
+      .option('-d, --debug', 'see full error messages, mostly for debugging');
+
+    program
+      .command('check', { isDefault: true })
+      .description('check if the project contains redundant polyfills that can be optimized by nolyfill')
+      .argument('[path]', 'project path to check')
+      .addOption(pmCommandOption)
+      .action(async (source: string | undefined, option: PmCommandOptions) => {
+        const projectPath = path.resolve(source ?? process.cwd());
+        const packageManager = option.pm === 'auto' ? await detectPackageManager(projectPath) : option.pm;
+
+        if (checkUnsupportedPM(packageManager)) {
+          return;
+        }
+
+        const packagesToBeOverride = await findPackagesCoveredByNolyfill(packageManager, projectPath);
+
+        if (packagesToBeOverride.length === 0) {
+          const packagesNotCoveredByNolyfill = await findPackagesNotCoveredByNolyfill(packageManager, projectPath);
+
+          if (packagesNotCoveredByNolyfill.length === 0) {
+            console.log(`${picocolors.green('Congratulations! Your project does not contain any redundant polyfills that can be optimized by nolyfill 🚀')}\n`);
+          } else {
+            await generateIssue(projectPath, packageManager, packagesNotCoveredByNolyfill);
+          }
+        } else {
+          console.log(picocolors.yellow(`Found ${picocolors.green(picocolors.bold(packagesToBeOverride.length))} redundant packages:`));
+          console.log(renderTree(packagesToBeOverride));
+
+          console.log(`Run "${picocolors.bold(picocolors.green('nolyfill install'))}" to replace them with a super lightweight ✨ version.\n`);
+        }
+      });
+
+    program
+      .command('install')
+      .description('replace redundant polyfills inside your project with nolyfill')
+      .argument('[path]', 'project path to install nolyfill into')
+      .addOption(pmCommandOption)
+      .action(async (source: string | undefined, option: PmCommandOptions) => {
+        const projectPath = path.resolve(source ?? process.cwd());
+        const packageManager = option.pm === 'auto' ? await detectPackageManager(projectPath) : option.pm;
+
+        if (checkUnsupportedPM(packageManager)) {
+          return;
+        }
+
+        const packagesToBeOverride = await findPackagesCoveredByNolyfill(packageManager, projectPath);
+
+        if (packagesToBeOverride.length === 0) {
+          const packagesNotCoveredByNolyfill = await findPackagesNotCoveredByNolyfill(packageManager, projectPath);
+
+          if (packagesNotCoveredByNolyfill.length === 0) {
+            console.log(`${picocolors.green('Congratulations! Your project does not contain any redundant polyfills that can be optimized by nolyfill 🚀')}\n`);
+          } else {
+            await generateIssue(projectPath, packageManager, packagesNotCoveredByNolyfill);
+          }
+        } else {
+          console.log(picocolors.yellow(`Found ${picocolors.green(picocolors.bold(packagesToBeOverride.length))} redundant packages:`));
+          console.log(renderTree(packagesToBeOverride));
+
+          await overridesPackageJson(packageManager, projectPath, packagesToBeOverride);
+
+          printPostInstallInstructions(packageManager);
+        }
+      });
+
+    await checkForUpdates('nolyfill', version);
+    await program.parseAsync(process.argv);
+  } catch (e) {
+    handleError(e as Error, !!program.opts<CliOptions>().debug);
+  }
+})();
