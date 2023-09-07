c8227581ede274cdea244d701f0af14a09e0b9e2
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-07 14:41:52 +0200

    feat: standalone version for each package (#23)
    Co-authored-by: SukkaW <isukkaw@gmail.com>

--- packages/tools/internal/index.js
+++ packages/tools/internal/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// @ts-check
 const fs = require('fs');
 const fsPromises = fs.promises;
 
@@ -7,22 +8,30 @@ const fsPromises = fs.promises;
  * @param {string} path
  */
 const fileExists = (path) => fsPromises.access(path, fs.constants.F_OK).then(() => true, () => false);
+
 /**
- * If filePath doesn't exist, create new file with content.
- * If filePath already exists, compare content with existing file, only update the file when content changes.
+ * - If filePath doesn't exist, create new file with content, then return true.
+ * - If filePath already exists, compare content with existing file, update the file when
+ *   content changed and return true, otherwise return false.
  *
  * @param {string} filePath
  * @param {string} fileContent
+ * @param {Set<string> | undefined} [existingFiles] - Set of existing files, if provided, will help speed up fileExists check.
+ * @returns {Promise<boolean>}
  */
-async function compareAndWriteFile(filePath, fileContent) {
-  if (await fileExists(filePath)) {
+async function compareAndWriteFile(filePath, fileContent, existingFiles) {
+  if ((existingFiles && existingFiles.has(filePath)) || await fileExists(filePath)) {
     const existingContent = await fsPromises.readFile(filePath, { encoding: 'utf8' });
     if (existingContent !== fileContent) {
-      return fsPromises.writeFile(filePath, fileContent, { encoding: 'utf-8' });
+      await fsPromises.writeFile(filePath, fileContent, { encoding: 'utf-8' });
+      return true;
     }
-  } else {
-    return fsPromises.writeFile(filePath, fileContent, { encoding: 'utf-8' });
+
+    return false;
   }
+
+  await fsPromises.writeFile(filePath, fileContent, { encoding: 'utf-8' });
+  return true;
 }
 
 module.exports.fileExists = fileExists;
