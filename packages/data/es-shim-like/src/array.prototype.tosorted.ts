82cdea495bf53ef2ca92d1561e109b3682f6b4f6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-10-10 10:57:25 +0800

    chore: migrate codegen (#38)

---
+++ packages/data/es-shim-like/src/array.prototype.tosorted.ts
@@ -0,0 +1,15 @@
+import { defineEsShim } from "@nolyfill/shared";
+
+const implementation = Array.prototype.toSorted
+  || function <T>(this: T[], compareFn: (a: T, b: T) => number) {
+    const o = Object(this);
+    const l = Number(o.length);
+    const a = new Array(l);
+    for (let i = 0; i < l; i++) {
+      a[i] = o[i];
+    }
+    Array.prototype.sort.call(a, compareFn);
+    return a;
+  }
+
+export default defineEsShim(implementation)
