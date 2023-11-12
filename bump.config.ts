16bb26261edfb1b9493f0b9cf04d1e22a6c7b436
SukkaW <isukkaw@gmail.com>
2023-11-13 00:02:51 +0800

    chore(release): auto bump `shared` version

--- bump.config.ts
+++ bump.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'bumpp';
 export default defineConfig({
   files: [
     'package.json',
-    'packages/tools/cli/package.json'
+    'packages/tools/cli/package.json',
+    'packages/tools/internal/package.json',
+    'packages/tools/shared/package.json'
   ]
 });
