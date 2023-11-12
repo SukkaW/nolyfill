cf8c42ddeb44959138860c1704fe193ce345b83d
AsukaMinato <i@asukaminato.eu.org>
2023-11-13 04:39:13 +0900

    string.prototype

---
+++ packages/generated/string.prototype.includes/index.js
@@ -0,0 +1,2 @@
+'use strict';
+module.exports = require('./entry.js').index();
---
+++ packages/generated/string.prototype.split/entry.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return t}});const e=require("@nolyfill/shared"),t=(0,e.defineEsShim)(String.prototype.split);
+Object.assign(exports.default, exports); module.exports = exports.default;
