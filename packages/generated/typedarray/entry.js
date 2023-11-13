ea5b94effe0cadc411009f143a08b163b476806d
AsukaMinato <i@asukaminato.eu.org>
2023-11-13 15:03:50 +0900

    mv typedarray

--- packages/generated/typedarray/entry.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const r=require("@nolyfill/shared"),e=(0,r.defineEsShim)({ArrayBuffer,DataView,Float32Array,Float64Array,Int8Array,Int16Array,Int32Array,Uint8Array,Uint8ClampedArray,Uint16Array,Uint32Array},!0);
-Object.assign(exports.default, exports); module.exports = exports.default;
--- packages/generated/typedarray/index.js
+++ packages/generated/typedarray/index.js
@@ -1,2 +1,2 @@
-'use strict';
-module.exports = require('./entry.js').index();
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const r=require("@nolyfill/shared"),e=(0,r.defineEsShim)({ArrayBuffer,DataView,Float32Array,Float64Array,Int8Array,Int16Array,Int32Array,Uint8Array,Uint8ClampedArray,Uint16Array,Uint32Array},!0);
+((typeof exports.default === 'object' && exports.default !== null) || typeof exports.default === 'function') && Object.assign(exports.default,exports); module.exports = exports.default;
