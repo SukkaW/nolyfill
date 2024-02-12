18babeeab3c631f29f33d806f99d5503cf682dae
SukkaW <isukkaw@gmail.com>
2024-02-12 22:33:08 +0800

    chore: housekeeping

--- packages/tools/cli/src/lockfile/yarn.ts
+++ packages/tools/cli/src/lockfile/yarn.ts
@@ -1,6 +1,6 @@
 import fsp from 'fs/promises';
 import path from 'path';
-import { parseSyml } from '@yarnpkg/parsers/lib/syml';
+import { parseSyml } from '@yarnpkg/parsers';
 
 import type { PackageNode } from '../types';
 import { cache } from '../lib/cache';
