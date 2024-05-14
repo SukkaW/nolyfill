35dd3c6caa135e49c4b690df09452c22e9701617
NamesMT <dangquoctrung123@gmail.com>
2024-05-14 05:46:44 +0000

    fix: support of lockfile v5

--- packages/tools/cli/src/lockfile/index.ts
+++ packages/tools/cli/src/lockfile/index.ts
@@ -2,6 +2,8 @@ import type { PackageManager } from '../package-manager';
 import { buildPNPMDepTree } from './pnpm';
 import { buildNPMDepTree } from './npm';
 import { buildYarnDepTree } from './yarn';
+import fsp from 'fs/promises';
+import path from 'path';
 
 export interface PackageLockDeps {
   [depName: string]: PackageLockDep
@@ -28,4 +30,14 @@ export function buildDepTrees(packageManager: PackageManager, dir: string) {
     default:
       throw new Error(`Unknown package manager: ${packageManager as string}`);
   }
+}
+
+export async function getPNPMLockfileVersion(dir: string) {
+  const content = await fsp.readFile(path.resolve(dir, 'pnpm-lock.yaml'), 'utf-8');
+  const [, lockfileVersion] = content.match(/^lockfileVersion: '?(\d*(?:.\d*)?)'?$/m) ?? [];
+
+  if (!lockfileVersion)
+    throw new Error(`Can't detect lockfile version`)
+
+  return lockfileVersion;
 }
