93c7c4aace6d8e51b4c734f4d6d84fe9d0df8a2b
Trung Dang <dangquoctrung123@gmail.com>
2024-06-23 16:50:34 +0700

    fix: support of lockfile v5 (#65)

--- packages/tools/cli/src/lockfile/pnpm.ts
+++ packages/tools/cli/src/lockfile/pnpm.ts
@@ -1,9 +1,11 @@
 import { searchForPackages } from '@pnpm/list';
+import { searchForPackages as searchForPackages_Old } from '@pnpm/list--old';
 import type { PackageNode } from '../types';
 import path from 'path';
 import { fileExists } from '@nolyfill/internal';
 import { findWorkspacePackagesNoCheck } from '@pnpm/workspace.find-packages';
 import { cache } from '../lib/cache';
+import { getPNPMLockfileVersion } from '.';
 
 export const buildPNPMDepTree = cache(async (dirPath: string): Promise<PackageNode[]> => {
   const dirPaths = [dirPath];
@@ -13,15 +15,30 @@ export const buildPNPMDepTree = cache(async (dirPath: string): Promise<PackageNo
     dirPaths.push(...allProjects.map((project) => project.dir));
   }
 
-  const result = await searchForPackages(['*'], dirPaths, {
+  const searchNew = () => searchForPackages(['*'], dirPaths, {
     depth: Infinity,
     lockfileDir: dirPath,
     include: {
       dependencies: true,
       devDependencies: true,
       optionalDependencies: false
-    }
-  });
+    },
+    virtualStoreDirMaxLength: Infinity
+  })
+
+  const searchOld = () => searchForPackages_Old(['*'], dirPaths, {
+    depth: Infinity,
+    lockfileDir: dirPath,
+    include: {
+      dependencies: true,
+      devDependencies: true,
+      optionalDependencies: false
+    },
+  })
+
+  const lockfileVersion = await getPNPMLockfileVersion(dirPath)
+
+  const result = await ((+lockfileVersion) < 6 ? searchOld() : searchNew())
 
   return result.flatMap((dep) => {
     return [...(dep.dependencies ?? []), ...(dep.devDependencies ?? [])];
