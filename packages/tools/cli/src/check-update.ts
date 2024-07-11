691cbef8a23bb7e7c7579369e66f755ed45b1804
Sukka <isukkaw@gmail.com>
2024-07-11 23:17:11 +0800

    perf: use `fast-npm-meta` (#83)

--- packages/tools/cli/src/check-update.ts
+++ packages/tools/cli/src/check-update.ts
@@ -2,11 +2,10 @@ import { join } from 'path';
 import fs from 'fs';
 import fsp from 'fs/promises';
 import { tmpdir } from 'os';
-import https from 'https';
-import http from 'http';
 import { env } from 'process';
 import picocolors from 'picocolors';
-import type { PackageJson } from 'type-fest';
+
+import { getLatestVersion } from 'fast-npm-meta';
 
 const UPDATE_CHECK_INTERVAL = 3_600_000;
 const UPDATE_CHECK_DIST_TAG = 'latest';
@@ -53,61 +52,6 @@ const updateCache = (file: string, latest: string | null, lastUpdate: number) =>
   return fsp.writeFile(file, content, 'utf8');
 };
 
-const loadPackage = (url: URL) => new Promise<PackageJson>((resolve, reject) => {
-  const options: https.RequestOptions = {
-    host: url.hostname,
-    path: url.pathname,
-    port: url.port,
-    headers: {
-      accept: 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
-    },
-    timeout: 2000
-  };
-
-  const { get } = url.protocol === 'https:' ? https : http;
-  get(options, response => {
-    const { statusCode } = response;
-
-    if (statusCode !== 200) {
-      const error = new Error(`Request failed with code ${statusCode}`);
-      Object.defineProperty(error, 'code', { value: statusCode });
-
-      reject(error);
-
-      // Consume response data to free up RAM
-      response.resume();
-      return;
-    }
-
-    let rawData = '';
-    response.setEncoding('utf8');
-
-    response.on('data', chunk => {
-      rawData += chunk;
-    });
-
-    response.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        resolve(parsedData);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }).on('error', reject).on('timeout', reject);
-});
-
-const getMostRecent = async (distTag: string): Promise<string | null> => {
-  const url = new URL(`https://registry.npmjs.org/nolyfill/${distTag}`);
-
-  try {
-    const spec = await loadPackage(url);
-    return spec.version ?? null;
-  } catch {
-    return null;
-  }
-};
-
 export const checkForUpdates = async (name: string, currentVersion: string): Promise<void> => {
   // Do not check for updates if the `NO_UPDATE_CHECK` variable is set.
   if (env.NO_UPDATE_CHECK) return;
@@ -128,7 +72,7 @@ export const checkForUpdates = async (name: string, currentVersion: string): Pro
   ({ shouldCheck, latest } = await evaluateCache(file, time, UPDATE_CHECK_INTERVAL));
 
   if (shouldCheck) {
-    latest = (await getMostRecent(UPDATE_CHECK_DIST_TAG)) ?? null;
+    latest = (await getLatestVersion(`nolyfill@${UPDATE_CHECK_DIST_TAG}`)).version;
     // If we pulled an update, we need to update the cache
     await updateCache(file, latest, time);
   }
