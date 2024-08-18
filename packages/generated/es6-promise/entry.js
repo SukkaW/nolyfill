ffd7c85ebc595894ca149806c97a1b93ddeb8189
Asuka Minato <i@asukaminato.eu.org>
2024-08-18 22:21:21 +0900

    try add Promise

---
+++ packages/generated/es6-promise/entry.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const e=(0,require("@nolyfill/shared").defineEsShim)(Promise);
+Object.assign(exports.default, exports); module.exports = exports.default;
