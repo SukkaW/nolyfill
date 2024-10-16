9fac3fbd8d394700f3739ad67389e45e28708eab
SukkaW <isukkaw@gmail.com>
2024-10-16 21:46:58 +0800

    fix(#104): proper `function.prototype.name` implementation

--- packages/generated/function.prototype.name/entry.js
+++ packages/generated/function.prototype.name/entry.js
@@ -1,2 +1,2 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const e=(0,require("@nolyfill/shared").defineEsShim)(Function.prototype.name);
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const e=(0,require("@nolyfill/shared").defineEsShim)(function(){return this.name});
 Object.assign(exports.default, exports); module.exports = exports.default;
