c8138ba3eda9844a0075db3af707600627dc1b11
Asuka Minato <i@asukaminato.eu.org>
2024-10-21 03:33:08 +0900

    rm outdated file

--- packages/generated/es6-promise/entry.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:function(){return e}});const e=(0,require("@nolyfill/shared").defineEsShim)(Promise);
-Object.assign(exports.default, exports); module.exports = exports.default;
